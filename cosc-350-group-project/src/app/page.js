30f2a7e5598b7e531fff889b630892b90f27fe6d
nikole-florence <nikole320320@gmail.com>
2025-02-12 15:30:09 -0600

    Home page layout

--- cosc-350-group-project/src/app/page.js
+++ cosc-350-group-project/src/app/page.js
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>SpaceHolder</h1>
+    <div className="Home">
+      <header className="Home-header">
+        <h1>The Forgotten Shelf</h1>
+      </header>
+
+      <div className="image-container">
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Placeholder"
+          className="App-logo"
+          />
+      </div>
+
+      <div>
+        <button className="button">Create Account</button>
+        <button className="button">Log In</button>
+      </div>
     </div>
   );
 }
